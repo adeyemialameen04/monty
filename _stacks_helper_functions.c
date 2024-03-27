8886d277fd4a8eea13a068700d7b9ae688dfcaa4
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-03-27 22:35:22 +0100

    bettyfied

--- _stacks_helper_functions.c
+++ _stacks_helper_functions.c
@@ -1,5 +1,10 @@
 #include "monty.h"
 
+/**
+ * is_empty - To check if the stack is empty.
+ * @stack: Double pointer to the stack.
+ * Return: True/False.
+ */
 bool is_empty(stack_t **stack)
 {
 	return (*stack == NULL);
